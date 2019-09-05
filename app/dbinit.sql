e4c1e301e5d467ae20c0bb86846c3f6bdf571697
ncrmns <polhossy@gmail.com>
2019-09-05 11:50:18 +0200

    fix(initdb.sql): card types with ENUM

--- app/dbinit.sql
+++ app/dbinit.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE erste;
 
-USE DATABASE erste;
+USE erste;
 
 CREATE TABLE bankcards (
 	card_id INT(7) NOT NULL AUTO_INCREMENT,
-	card_type VARCHAR(40) NOT NULL,
+	card_type ENUM('Mastercard Standard', 'Maestro', 'Mastercard Gold', 'Mastercard World Gold', 'Mastercard Standard Devisa', 'Maestro Student', 'Visa Classic', 'Visa Virtual', 'Visa Electron' ) NOT NULL,
 	card_num INT(16) NOT NULL,
 	card_valid VARCHAR(5) NOT NULL,
 	card_hash VARCHAR(150) NOT NULL,
@@ -14,8 +14,12 @@ CREATE TABLE bankcards (
 
 CREATE TABLE contact (
 	contact_id INT(7) NOT NULL AUTO_INCREMENT,
-	card_id` INT(7) NOT NULL,
+	card_id INT(7) NOT NULL,
 	contact_type VARCHAR(20) NOT NULL,
 	contact_data VARCHAR(40) NOT NULL,
-	PRIMARY KEY (contact_id)
+	PRIMARY KEY (contact_id),
+	CONSTRAINT fk_card_id 
+	FOREIGN KEY (card_id)
+	REFERENCES bankcards (card_id)
+	ON DELETE CASCADE
 );
