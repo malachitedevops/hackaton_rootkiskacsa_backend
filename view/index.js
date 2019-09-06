1214519ecec1a35b3aa23cbe92ec97618c5aeee7
SamuZad <zadori.samu@gmail.com>
2019-09-06 04:06:24 +0200

    Feat(index.js): added additional interactivity

--- view/index.js
+++ view/index.js
@@ -7,7 +7,6 @@ let blockCard = document.getElementById("blockCard");
 let checkValidity = document.getElementById("checkValidity");
 let h2 = document.querySelector("h2")
 
-
 queryCard.addEventListener('click', (event) =>{
 event.preventDefault();
 h2.innerHTML = "Query"
@@ -43,28 +42,73 @@ checkValidity.addEventListener('click', (event)=>{
   event.preventDefault();
   h2.innerHTML = "Check Validity"
   let div = document.querySelector("div");
-  div.innerHTML=`Card Number: <input type="text" name="cardNumber"><br>`
+  div.innerHTML=`Card Type: <input type="text" name="cardType"><br>
+  Card Number: <input type="text" name="cardNumber"><br>
+  Valid through: <input type="text" name="validThru"><br>
+  CVV: <input type="password" name="CVV"><br>`
 })
 
-button.addEventListener('click', (event) => Register())
+button.addEventListener('click', (event) => send())
 
 
-const Register = function() {
-  event.preventDefault();
-  fetch('http://54.81.110.184:3000/ecards', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    mode: "same-origin",
-    body: JSON.stringify({
-      "cardType": document.querySelectorAll("input")[0].value,
-      "cardNumber": document.querySelectorAll("input")[1].value,
-      "validThru": document.querySelectorAll("input")[2].value,
-      "CVV": document.querySelectorAll("input")[3].value,
-      "owner": document.querySelectorAll("input")[4].value,
-      "contactInfo": document.querySelectorAll("input")[5].value,
-      "contactType": document.querySelectorAll("input")[6].value
-    }),
-  })
+const send = function() {
+  if(h2.innerHTML==='Add Card'){
+    event.preventDefault();
+    fetch('http://localhost:3000/ecards', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: "same-origin",
+      body: JSON.stringify({
+        "username":document.querySelectorAll("input")[0].value,
+        "password":document.querySelectorAll("input")[1].value,
+        "cardType": document.querySelectorAll("input")[2].value,
+        "cardNumber": document.querySelectorAll("input")[3].value,
+        "validThru": document.querySelectorAll("input")[4].value,
+        "CVV": document.querySelectorAll("input")[5].value,
+        "owner": document.querySelectorAll("input")[6].value,
+        "contactInfo": document.querySelectorAll("input")[7].value,
+        "contactType": document.querySelectorAll("input")[8].value
+      }),
+    })
+  }else if(h2.innerHTML==="Query"){
+    event.preventDefault();
+    fetch(`http://localhost:3000/ecards/${document.querySelectorAll('input')[0].value}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: "same-origin",
+    })
+  }else if(h2.innerHTML='Check Validity'){
+    event.preventDefault();
+    fetch('http://localhost:3000/validate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: "same-origin",
+      body: JSON.stringify({
+        "cardType": document.querySelectorAll("input")[0].value,
+        "cardNumber": document.querySelectorAll("input")[1].value,
+        "validThru": document.querySelectorAll("input")[2].value,
+        "CVV": document.querySelectorAll("input")[3].value,
+      }),
+    })
+  }else if(h2.innerHTML==='Block Card'){
+    event.preventDefault();
+    fetch(`http://localhost:3000/ecards/${document.querySelectorAll("input")[2].value}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: "same-origin",
+      body: JSON.stringify({
+        "username": document.querySelectorAll("input")[0].value,
+        "password": document.querySelectorAll("input")[1].value,
+      }),
+    })
+  }
+
  }
