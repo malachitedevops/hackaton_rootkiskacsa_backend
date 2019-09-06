6a13ff9dd1b06acf2b951cf760c2a3b08c9863fc
SamuZad <zadori.samu@gmail.com>
2019-09-06 08:38:54 +0200

    refactor

--- view/index.js
+++ view/index.js
@@ -11,61 +11,41 @@ queryCard.addEventListener('click', (event) =>{
 event.preventDefault();
 h2.innerHTML = "Query"
 let div = document.querySelector("div");
-div.innerHTML= 'CardNumber: <input type="number" name="cardNumber" class = "choice"><br>';
+div.innerHTML= 'CardNumber: <input type="number" name="cardNumber"><br>';
 })
 
 addCard.addEventListener('click', (event)=>{
 event.preventDefault();
 h2.innerHTML = "Add Card"
 let div = document.querySelector("div");
-div.innerHTML=`Username: <input type="text" name="username" class = "choice"><br>
-Password: <input type="password" name="password" class = "choice"><br>
-Card Type: <select name="cardType" class = "choice">
-        <option value='Mastercard Standard'>Mastercard Standard</option>
-        <option value='Maestro'>Maestro</option>
-        <option value='Mastercard Gold'>Mastercard Gold</option>
-        <option value='Mastercard World Gold'>Mastercard World Gold</option>
-        <option value="Mastercard Standard Devisa">Mastercard Standard Devisa</option>
-        <option value='Maestro Student'>Maestro Student</option>
-        <option value='Visa Classic'>Visa Classic'</option>
-        <option value='Visa Virtual'>Visa Virtual</option>
-        <option value='Visa Electron'>Visa Electron</option>
-      </select><br>
-CardNumber: <input type="number" name="cardNumber" class = "choice"><br>
-Valid through: <input type="text" name="validThru" class = "choice"><br>
-CVV: <input type="password" name="CVV" class = "choice"><br>
-Owner: <input type="text" name="owner" class = "choice"><br>
-Contact info: <input type="text" name="contactInfo" class = "choice"><br>
-Contact type: <input type="text" name="contactType" class = "choice"><br>`
+div.innerHTML=`Username: <input type="text" name="username"><br>
+Password: <input type="password" name="password"><br>
+Card Type: <input type="text" name="cardType"><br>
+CardNumber: <input type="number" name="cardNumber"><br>
+Valid through: <input type="text" name="validThru"><br>
+CVV: <input type="password" name="CVV"><br>
+Owner: <input type="text" name="owner"><br>
+Contact info: <input type="text" name="contactInfo"><br>
+Contact type: <input type="text" name="contactType"><br>`
 })
 
 blockCard.addEventListener('click', (event) =>{
   event.preventDefault();
   h2.innerHTML = "Block Card"
   let div = document.querySelector("div");
-  div.innerHTML=`Username: <input type="text" name="username" class = "choice"><br>
-  Password: <input type="password" name="password" class = "choice"><br>
-  Card Number: <input type="text" name="cardNumber" class = "choice"><br>`
+  div.innerHTML=`Username: <input type="text" name="username"><br>
+  Password: <input type="password" name="password"><br>
+  Card Number: <input type="text" name="cardNumber"><br>`
 })
 
 checkValidity.addEventListener('click', (event)=>{
   event.preventDefault();
   h2.innerHTML = "Check Validity"
   let div = document.querySelector("div");
-  div.innerHTML=`Card Type: <select name="cardType" class = "choice">
-  <option value='Mastercard Standard'>Mastercard Standard </option>
-  <option value='Maestro'>Maestro</option>
-  <option value='Mastercard Gold'>Mastercard Gold</option>
-  <option value='Mastercard World Gold'>Mastercard World Gold</option>
-  <option value="Mastercard Standard Devisa">Mastercard Standard Devisa</option>
-  <option value='Maestro Student'>Maestro Student</option>
-  <option value='Visa Classic'>Visa Classic'</option>
-  <option value='Visa Virtual'>Visa Virtual</option>
-  <option value='Visa Electron'>Visa Electron</option>
-</select><br>
-  Card Number: <input type="text" name="cardNumber" class = "choice"><br>
-  Valid through: <input type="text" name="validThru" class = "choice"><br>
-  CVV: <input type="password" name="CVV" class = "choice"><br>`
+  div.innerHTML=`Card Type: <input type="text" name="cardType"><br>
+  Card Number: <input type="text" name="cardNumber"><br>
+  Valid through: <input type="text" name="validThru"><br>
+  CVV: <input type="password" name="CVV"><br>`
 })
 
 button.addEventListener('click', (event) => send())
@@ -81,15 +61,15 @@ const send = function() {
       },
       mode: "same-origin",
       body: JSON.stringify({
-        "username":document.querySelectorAll(".choice")[0].value,
-        "password":document.querySelectorAll(".choice")[1].value,
-        "cardType": document.querySelectorAll(".choice")[2].value,
-        "cardNumber": document.querySelectorAll(".choice")[3].value,
-        "validThru": document.querySelectorAll(".choice")[4].value,
-        "CVV": document.querySelectorAll(".choice")[5].value,
-        "owner": document.querySelectorAll(".choice")[6].value,
-        "contactInfo": document.querySelectorAll(".choice")[7].value,
-        "contactType": document.querySelectorAll(".choice")[8].value
+        "username":document.querySelectorAll("input")[0].value,
+        "password":document.querySelectorAll("input")[1].value,
+        "cardType": document.querySelectorAll("input")[2].value,
+        "cardNumber": document.querySelectorAll("input")[3].value,
+        "validThru": document.querySelectorAll("input")[4].value,
+        "CVV": document.querySelectorAll("input")[5].value,
+        "owner": document.querySelectorAll("input")[6].value,
+        "contactInfo": document.querySelectorAll("input")[7].value,
+        "contactType": document.querySelectorAll("input")[8].value
       }),
     })
 			.then(response => response.json())
@@ -106,15 +86,15 @@ const send = function() {
 			.then(data => console.log(data))
   }else if(h2.innerHTML==='Block Card'){
     event.preventDefault();
-    fetch(`http://3.87.38.201:3000/ecards/${document.querySelectorAll(".choice")[2].value}`, {
+    fetch(`http://3.87.38.201:3000/ecards/${document.querySelectorAll("input")[2].value}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json'
       },
       mode: "same-origin",
       body: JSON.stringify({
-        "username": document.querySelectorAll(".choice")[0].value,
-        "password": document.querySelectorAll(".choice")[1].value,
+        "username": document.querySelectorAll("input")[0].value,
+        "password": document.querySelectorAll("input")[1].value,
       }),
     })
   }else if(h2.innerHTML='Check Validity'){
@@ -126,10 +106,10 @@ const send = function() {
       },
       mode: "same-origin",
       body: JSON.stringify({
-        "cardType": document.querySelectorAll(".choice")[0].value,
-        "cardNumber": document.querySelectorAll(".choice")[1].value,
-        "validThru": document.querySelectorAll(".choice")[2].value,
-        "CVV": document.querySelectorAll(".choice")[3].value,
+        "cardType": document.querySelectorAll("input")[0].value,
+        "cardNumber": document.querySelectorAll("input")[1].value,
+        "validThru": document.querySelectorAll("input")[2].value,
+        "CVV": document.querySelectorAll("input")[3].value,
       }),
     })
   }
